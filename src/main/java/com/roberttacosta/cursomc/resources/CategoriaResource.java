d0130049d0a78a2feb24bd689b8b96f7d389ab8a
Robertta Costa <roberttaloise@hotmail.com>
2020-01-13 15:22:27 -0300

    Criando operação de instanciacao

--- src/main/java/com/roberttacosta/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/roberttacosta/cursomc/resources/CategoriaResource.java
@@ -1,10 +1,5 @@
 package com.roberttacosta.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.xml.ws.Response;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/com/roberttacosta/cursomc/services/CategoriaService.java
+++ src/main/java/com/roberttacosta/cursomc/services/CategoriaService.java
@@ -1,10 +1,8 @@
 package com.roberttacosta.cursomc.services;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 import com.roberttacosta.cursomc.domain.Categoria;
