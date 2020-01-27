ced25cbd388678f9891fe29446d8d94a542dd526
Robertta Costa <roberttaloise@hotmail.com>
2020-01-27 09:26:12 -0300

    validacao sintatica com Bean Validation

--- src/main/java/com/roberttacosta/cursomc/services/CategoriaService.java
+++ src/main/java/com/roberttacosta/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.roberttacosta.cursomc.domain.Categoria;
+import com.roberttacosta.cursomc.dto.CategoriaDTO;
 import com.roberttacosta.cursomc.exceptions.DataIntegrityException;
 import com.roberttacosta.cursomc.exceptions.ObjectNotFoundExcepetion;
 import com.roberttacosta.cursomc.repositories.CategoriaRepository;
@@ -54,6 +55,10 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
 				orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
