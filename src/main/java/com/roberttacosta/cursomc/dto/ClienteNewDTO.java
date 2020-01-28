f7ab0931f9d0529e19dbd0be169536fac27c6e0a
Robertta Costa <roberttaloise@hotmail.com>
2020-01-28 10:45:29 -0300

    Validacao customizada de CPF ou CNPJ na inserção de cliente

--- src/main/java/com/roberttacosta/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/roberttacosta/cursomc/dto/ClienteNewDTO.java
@@ -2,13 +2,51 @@ package com.roberttacosta.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.roberttacosta.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
-	private String nome, email, cpfOuCnpj;
+	@NotEmpty (message = "Preenchimento obrigatório")
+	@Length (min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres")
+	private String nome;
+	
+	@NotEmpty (message = "Preenchimento obrigatório")
+	@Email (message = "Email inválido")
+	private String email;
+	
+	@NotEmpty (message = "Preenchimento obrigatório")
+	
+	private String cpfOuCnpj;
+	
 	private Integer tipo;
-	private String logradouro, numero, complemento, bairro, cep;
-	private String telefone1, telefone2, telefone3;
+	
+	@NotEmpty (message = "Preenchimento obrigatório")
+	private String logradouro;
+	
+	@NotEmpty (message = "Preenchimento obrigatório")
+	private String numero;
+	
+	private String complemento; 
+	
+	private String bairro; 
+	
+	@NotEmpty (message = "Preenchimento obrigatório")
+	private String cep;
+	
+	@NotEmpty (message = "Preenchimento obrigatório")
+	private String telefone1;
+	
+	private String telefone2; 
+	
+	private String telefone3;
+	
 	private Integer cidadeId;
 	
 	public ClienteNewDTO() {
