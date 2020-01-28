613950b6e64aa8fcfcc970b944a39127de354613
Robertta Costa <roberttaloise@hotmail.com>
2020-01-28 11:13:18 -0300

    Validacao customizada email nao repetido na atualizacao de cliente

--- src/main/java/com/roberttacosta/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/roberttacosta/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.roberttacosta.cursomc.domain.Cliente;
+import com.roberttacosta.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
