3362fe308e99aa70319cad0370b7150832702762
Robertta Costa <roberttaloise@hotmail.com>
2020-01-28 09:28:43 -0300

    Ajustando e testando a delecao de cliente

--- src/main/java/com/roberttacosta/cursomc/resources/ClienteResource.java
+++ src/main/java/com/roberttacosta/cursomc/resources/ClienteResource.java
@@ -17,9 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.roberttacosta.cursomc.domain.Categoria;
 import com.roberttacosta.cursomc.domain.Cliente;
-import com.roberttacosta.cursomc.dto.CategoriaDTO;
 import com.roberttacosta.cursomc.dto.ClienteDTO;
 import com.roberttacosta.cursomc.dto.ClienteNewDTO;
 import com.roberttacosta.cursomc.services.ClienteService;
